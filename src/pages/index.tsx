3fc7accf7640160fbb7dfa9b0891934c1d8c966a
try <try418@163.com>
2023-05-27 00:21:50 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: try try418@163.com
+ * @Date: 2023-05-27 00:09:47
+ * @Description:
+ */
+export default function HomePage() {
+  return (
+    <div>
+      <h2>try! Welcome to umi!</h2>
+    </div>
+  );
+}
